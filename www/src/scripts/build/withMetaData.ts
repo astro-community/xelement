0a9aba81a61aa823d3fae60c069a1a406947e9b3
aFuzzyBear <afuzzybear@outlook.com>
2022-03-20 01:40:41 +0000

    Working on the routing

--- www/src/scripts/build/withMetaData.ts
+++ www/src/scripts/build/withMetaData.ts
@@ -1,29 +1,28 @@
-    
-const fetchData = async(path)=>{
-    return await import(path).then(exports=>exports.frontmatter)
-  }
+
   /** Attaches frontmatter and data from each file alongside nested menu of static paths. */
   const withMetaData = async (paths) => {
+      const routes = [...paths]
       const hash = Object.create(null)
       const root = { name: 'root' }
-      for (const path of paths) {
-          const data = await fetchData(path.props.path)
+      for (const path of routes) {
+        const {slug,directory,data} = path.props
           const metaData = Object.entries(data)
               .filter(result=>!result.includes('astro'))
               .reduce((prev,current)=>({...prev,[current[0]]:current[1]}),{})
-          path.props.data = data
-          const segments = path.props.slug.split('/')
-          const filtered = segments.filter(x=> x!=='').filter(x=> !(x.includes('readme')))
           let item = root
-          for (const [index, name] of Object.entries(filtered)) {
+          for (const [index, name] of Object.entries(directory)) {
             if(!item.items){
                 item.items = []
             }
-            let isItem = index === String(filtered.length - 1)
+            let isItem = index === String(directory.length - 1)
             if (isItem) {
                 item.items.push({ 
-                    name, 
-                    href: `/docs/${path.props.directory}/${path.props.slug.toLowerCase()}`,
+                    name:slug,
+                    directory,
+                    href: `/docs/${directory.length > 1 
+                            ? directory.reduce((acc,curr)=>
+                                 acc+=`${curr}/`,'') 
+                            : `${directory}/`}${slug}`, 
                     meta:{...metaData}
                     })
             } else {
@@ -32,7 +31,7 @@ const fetchData = async(path)=>{
                 item = (
                 hash[name] = (
                     hash[name] ||
-                    item.items[item.items.push({ name, items: [] }) - 1]
+                    item.items[item.items.push({ directory:name,isDir:true, items: [] }) - 1]
                 )
                 )
             }
@@ -40,7 +39,7 @@ const fetchData = async(path)=>{
     
           path.props.menu = root.items
       }
-    return paths
+    return routes
   }
   
   export default withMetaData
